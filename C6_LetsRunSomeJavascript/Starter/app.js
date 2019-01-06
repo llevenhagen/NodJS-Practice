0a0f7c517f9256299b37b1703e6bd91a33a411ca
Laura Levenhagen <Laura@Lauras-MacBook-Pro.local>
2019-01-06 14:43:48 -0800

    finished lecture 64

--- C6_LetsRunSomeJavascript/Starter/app.js
+++ C6_LetsRunSomeJavascript/Starter/app.js
@@ -1,8 +1,17 @@
 let http = require('http');
 let fs = require('fs');
 http.createServer(function(req, res){
-
-  res.writeHead(200, { 'Content-Type': 'text/html'});
-  fs.createReadStream(__dirname + '/index.html').pipe(res)
-
+  if (req.url === '/'){
+    fs.createReadStream(__dirname + '/index.html').pipe(res)  }
+  else if (req.url === '/api'){
+  res.writeHead(200, { 'Content-Type': 'application/json'});
+  let obj = {
+    firstname: 'John',
+    lastname: 'Doe'
+  };
+  res.end(JSON.stringify(obj));
+} else {
+res.writeHead(404);
+res.end()
+}
 }).listen(3000, '127.0.0.1');
