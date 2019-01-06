1f95cad9ffd129420e6f18ef0f09ba0c57873cce
Laura Levenhagen <Laura@Lauras-MacBook-Pro.local>
2019-01-06 14:22:46 -0800

    completed lesson 60

--- C6_LetsRunSomeJavascript/Starter/app.js
+++ C6_LetsRunSomeJavascript/Starter/app.js
@@ -1,7 +1,12 @@
-var http = require('http');
-
+let http = require('http');
+let fs = require('fs');
 http.createServer(function(req, res){
 
-  res.writeHead(200, { 'Content-Type': 'text/plain'});
-  res.end('Hello world\n');
+  res.writeHead(200, { 'Content-Type': 'text/html'});
+  let html = fs.readFileSync(__dirname + '/index.html',
+'utf8');
+  let  message = 'Hello World...';
+  html = html.replace('{Message}', message);
+  res.end(html);
+
 }).listen(3000, '127.0.0.1');
