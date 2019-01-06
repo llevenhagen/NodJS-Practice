73f7fb4426751c626f5b34bc4d511c7b59b76aca
Laura Levenhagen <Laura@Lauras-MacBook-Pro.local>
2019-01-06 14:12:23 -0800

    finished lecture 59

---
+++ C6_LetsRunSomeJavascript/Starter/app.js
@@ -0,0 +1,7 @@
+var http = require('http');
+
+http.createServer(function(req, res){
+
+  res.writeHead(200, { 'Content-Type': 'text/plain'});
+  res.end('Hello world\n');
+}).listen(3000, '127.0.0.1');
