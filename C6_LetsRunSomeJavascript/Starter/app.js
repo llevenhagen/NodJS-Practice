1f2d2d63a24c568e218c97f823901f5a3d1c140d
Laura Levenhagen <Laura@Lauras-MacBook-Pro.local>
2019-01-06 16:15:43 -0800

    started node.js server and added express

--- C6_LetsRunSomeJavascript/Starter/app.js
+++ C6_LetsRunSomeJavascript/Starter/app.js
@@ -1,17 +1,36 @@
-let http = require('http');
-let fs = require('fs');
-http.createServer(function(req, res){
-  if (req.url === '/'){
-    fs.createReadStream(__dirname + '/index.html').pipe(res)  }
-  else if (req.url === '/api'){
-  res.writeHead(200, { 'Content-Type': 'application/json'});
-  let obj = {
-    firstname: 'John',
-    lastname: 'Doe'
-  };
-  res.end(JSON.stringify(obj));
-} else {
-res.writeHead(404);
-res.end()
-}
-}).listen(3000, '127.0.0.1');
+const moment = require('moment');
+const http = require('http');
+const fs = require('fs');
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+
+app.get('/', (req, res)=>{
+  res.render('index.html')
+})
+app.get('/api', (req, res)=> {
+  res.json({firstname: 'john', lastname: 'doe'})
+})
+app.get('/person/:id', (req, res)=> {
+  res.send('<h1>hey ' + req.params.id + '</h1>')
+})
+app.listen(port);
+// http.createServer(function(req, res) {
+//   if (req.url === '/') {
+//     fs.createReadStream(__dirname + '/index.html').pipe(res)
+//   } else if (req.url === '/api') {
+//     res.writeHead(200, {
+//       'Content-Type': 'application/json'
+//     });
+//     let obj = {
+//       firstname: 'John',
+//       lastname: 'Doe'
+//     };
+//     res.end(JSON.stringify(obj));
+//   } else {
+//     res.writeHead(404);
+//     res.end()
+//   }
+// }).listen(3000, '127.0.0.1');
